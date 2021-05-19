1fe7fdcf3bcf27a5c5b5982433096b7eca7a4ec0
BenediktLeonhardi <beneleo@gmx.de>
2021-05-19 09:18:34 +0200

    Neue Felder

---
+++ SRAßE.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse SRAßE.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class SRAßE extends FELD
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse SRAßE
+     */
+    public SRAßE()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
